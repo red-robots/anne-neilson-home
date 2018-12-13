17e6a07e35e94526517a26858b0a6d92a9e45e8a
Austin Crane <austincrane@gmail.com>
2018-12-13 12:50:37 -0500

    add google maps api key

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 
-
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBo3boBSxHZrgRAS_3Vx3-5IKnvaTysmCg"></script>
 
 <?php wp_head(); ?>
 </head>
